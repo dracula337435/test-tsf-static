97aaeda51375929b683952107285d62cfd49b8ec
dk <dracula337435@163.com>
2021-05-18 10:11:29 +0800

    增加提示的辨识度

--- src/main/java/io/dracula/test/tsf/static1/AStaticOne.java
+++ src/main/java/io/dracula/test/tsf/static1/AStaticOne.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class AStaticOne {
 
     static{
-        System.out.println("hello");
+        System.out.println("===hello===");
     }
 
     public static Set<String> someSet = new HashSet<>();
